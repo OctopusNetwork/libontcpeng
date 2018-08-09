a67a9788fdec13a73c3ea5b45d372af5729f5531
Zhao KK <yubin.zhao@yottatech.ai>
2018-08-09 09:54:26 +0800

    Remove old compile structure

--- project/source.mk
+++
@@ -1,9 +0,0 @@
-COMMON_INCLUDE_DIRS += $(rootdir)/$(MODULE)/include         \
-                       $(incdir)/libonutils                   \
-                       $(incdir)/liboncommunication              \
-                       $(incdir)/libonevent                   \
-                       $(incdir)/libonplatform
-
-COMMON_SRC_FILES := $(rootdir)/$(MODULE)/src/tcp_server.c
-
-COMMON_INST_HEADER_DIRS += $(rootdir)/$(MODULE)/include
